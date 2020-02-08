5e0a47773483f67a219702f623dde593c443dd11
tommy3wood <thomaswcurran1014@gmail.com>
2020-02-08 11:37:22 -0600

    added edit/update/destroy

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -20,7 +20,8 @@ class Client::ProductsController < ApplicationController
                           "http://localhost:3000/api/products",
                           form: client_params
                           )
-    render 'create.html.erb'
+    product = response.parse
+    redirect_to "/client/products/#{ product["id"] }"
   end
 
   def show
@@ -28,4 +29,31 @@ class Client::ProductsController < ApplicationController
     @product = response.parse
     render 'show.html.erb'
   end
+
+  def edit
+    response = HTTP.get("http://localhost:3000/api/products/#{params[:id]}")
+    @product = response.parse
+    render 'edit.html.erb'
+  end
+
+  def update
+    client_params = {
+                      name: params[:name],
+                      price: params[:price],
+                      description: params[:description],
+                      image_url: params[:image_url],
+                      }
+    response = HTTP.patch(
+                          "http://localhost:3000/api/products/#{params[:id]}",
+                          form: client_params
+                          )
+    product = response.parse
+    redirect_to "/client/products/#{ product["id"] }"
+  end
+
+  def destroy
+    response = HTTP.delete("http://localhost:3000/api/products/#{params[:id]}")
+    redirect_to '/client/products'
+  end
+
 end
